d1975a78a4ef15a8488242a5aaa983c099444022
mike7051141 <mike705114@gmail.com>
2023-09-27 23:19:30 +0900

    RestMap 9/27

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -10,19 +10,17 @@ import NaverMapView, {
 } from 'react-native-nmap';
 
 function MapPage() {
-  const P0 = {latitude: 37.564362, longitude: 126.977011};
-  const P1 = {latitude: 37.565051, longitude: 126.978567};
-  const P2 = {latitude: 37.565383, longitude: 126.976292};
+  const P0 = {latitude: 37.27566, longitude: 127.13245};
 
-  const [currentLocation, setCurrentLocation] = useState(null);
-  const [forceUpdate, setForceUpdate] = useState(false);
+  const [currentLocation, setCurrentLocation] = useState(P0); // 초기 값으로 P0를 설정합니다.
 
+  // 사용자의 현재 위치를 가져오는 함수
   const getCurrentLocation = () => {
     Geolocation.getCurrentPosition(
       position => {
         const {latitude, longitude} = position.coords;
         setCurrentLocation({latitude, longitude});
-        setForceUpdate(!forceUpdate); // 상태 업데이트로 인한 강제 리렌더링
+        console.log('업데이트');
       },
       error => {
         console.error('Error getting current location: ', error);
@@ -32,6 +30,15 @@ function MapPage() {
 
   useEffect(() => {
     getCurrentLocation();
+
+    // 3초마다 현재 위치 업데이트
+    const updateLocationInterval = setInterval(() => {
+      getCurrentLocation();
+    }, 3000);
+
+    return () => {
+      clearInterval(updateLocationInterval); // 컴포넌트 언마운트 시 타이머 해제
+    };
   }, []);
 
   return (
@@ -39,26 +46,39 @@ function MapPage() {
       <NaverMapView
         style={{flex: 1}}
         showsMyLocationButton={true}
-        center={currentLocation || P0}>
-        <Marker coordinate={P0} />
-        <Marker coordinate={P1} pinColor="blue" />
-        <Marker coordinate={P2} pinColor="red" />
-        <Path coordinates={[P0, P1]} width={10} />
-        <Polyline coordinates={[P1, P2]} />
-        <Circle coordinate={P0} color={'rgba(255,0,0,0.3)'} radius={200} />
-        <Polygon coordinates={[P0, P1, P2]} color={`rgba(0, 0, 0, 0.5)`} />
+        center={{
+          zoom: 11,
+          latitude: currentLocation.latitude,
+          longitude: currentLocation.longitude,
+          //latitude: (currentLocation.latitude + P0.latitude) / 2,
+          //longitude: (currentLocation.longitude + P0.longitude) / 2,
+        }} // 중심 위치를 현재 위치로 설정합니다.
+      >
+        {/* 현재 위치 마커 추가 */}
+        <Marker
+          coordinate={{
+            latitude: currentLocation.latitude,
+            longitude: currentLocation.longitude,
+          }}
+          caption="현재 위치"
+        />
+        <Marker
+          coordinate={P0}
+          pinColor="red"
+          onClick={() => console.warn('강남대!')}
+        />
       </NaverMapView>
       <TouchableOpacity
-        onPress={getCurrentLocation}
+        onPress={() => getCurrentLocation()} // 버튼을 누를 때 현재 위치 업데이트
         style={{
           position: 'absolute',
           bottom: 20,
-          left: 20,
-          backgroundColor: 'cyan',
+          right: 20,
+          backgroundColor: 'blue',
           padding: 10,
           borderRadius: 5,
         }}>
-        <Text style={{color: 'white'}}>짜증나</Text>
+        <Text style={{color: 'white'}}>Update Current Location</Text>
       </TouchableOpacity>
     </View>
   );
--- src/pages/RestMapPage.tsx
+++ src/pages/RestMapPage.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, ScrollView, Dimensions} from 'react-native';
-import NaverMapView from 'react-native-nmap';
+import NaverMapView, {Marker} from 'react-native-nmap';
+import Geolocation from '@react-native-community/geolocation';
 
 function RestMapPage() {
+  const P0 = {latitude: 37.27566, longitude: 127.13245};
+  const [currentLocation, setCurrentLocation] = useState(P0); // 초기 값으로 P0를 설정합니다.
+
+  // 사용자의 현재 위치를 가져오는 함수
+  const getCurrentLocation = () => {
+    Geolocation.getCurrentPosition(
+      position => {
+        const {latitude, longitude} = position.coords;
+        setCurrentLocation({latitude, longitude});
+        console.log('업데이트');
+      },
+      error => {
+        console.error('Error getting current location: ', error);
+      },
+    );
+  };
+
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
   return (
     <View
       style={{
@@ -10,8 +31,29 @@ function RestMapPage() {
         height: Dimensions.get('window').height,
       }}>
       <NaverMapView
-        style={{width: '100%', height: '80%'}}
-        zoomControl={false}></NaverMapView>
+        style={{flex: 1}}
+        showsMyLocationButton={true}
+        center={{
+          zoom: 11,
+          latitude: currentLocation.latitude,
+          longitude: currentLocation.longitude,
+          //latitude: (currentLocation.latitude + P0.latitude) / 2,
+          //longitude: (currentLocation.longitude + P0.longitude) / 2,
+        }} // 중심 위치를 현재 위치로 설정합니다.
+      >
+        <Marker
+          coordinate={{
+            latitude: currentLocation.latitude,
+            longitude: currentLocation.longitude,
+          }}
+          caption="현재 위치"
+        />
+        <Marker
+          coordinate={P0}
+          pinColor="red"
+          onClick={() => console.warn('강남대!')}
+        />
+      </NaverMapView>
     </View>
   );
 }
