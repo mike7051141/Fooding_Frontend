bc6b1b5b26c43ef0e48c46e9dbcac4c58337f7c6
mike7051141 <mike705114@gmail.com>
2023-11-08 02:45:25 +0900

    주소 -> 좌표 변환 env 환경설정

--- src/pages/AddRestWritePage.tsx
+++ src/pages/AddRestWritePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
@@ -7,12 +7,16 @@ import {
   TextInput,
   StyleSheet,
   Image,
+  TouchableOpacity,
+  Button,
 } from 'react-native';
 
 import DismissKeyboardView from '../components/DissmissKeyboardView';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {retrieveToken} from '../store/storage';
+import axios from 'axios';
 
+import {API_KEY} from '@env';
 const Token = async () => {
   const token = await retrieveToken();
   if (token) {
@@ -23,6 +27,36 @@ const Token = async () => {
 };
 
 function AddRestWritePage() {
+  const [locationInfo, setLocationInfo] = useState<string | null>(null);
+  const [addressInput, setAddressInput] = useState<string>(''); // State to store the input address
+  const [lat, setLat] = useState<string>(''); //위도
+  const [long, setLong] = useState<string>(''); //경도
+
+  async function geocodeAddress(address: string, apiKey: string) {
+    try {
+      const response = await axios.get(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${apiKey}`,
+      );
+
+      if (response.data.status === 'OK') {
+        const location = response.data.results[0].geometry.location;
+        const {lat, lng} = location;
+        const info = `주소: ${address} - 위도: ${lat}, 경도: ${lng}`;
+        setLocationInfo(info);
+        setLong(lng);
+        setLat(lat);
+        console.log(info);
+      } else {
+        console.error(response.data);
+      }
+    } catch (error) {
+      console.error('API 호출 중 오류 발생:', error);
+    }
+  }
+
+  const handleButtonClick = () => {
+    geocodeAddress(addressInput, API_KEY);
+  };
   return (
     <ScrollView style={{backgroundColor: 'white'}}>
       <DismissKeyboardView>
@@ -36,10 +70,27 @@ function AddRestWritePage() {
         <View style={styles.View1}>
           <Ionicons name="map-outline" size={25} color={'black'} />
           <Text style={styles.text}>주소를 입력해 주세요</Text>
+          <View style={{flex: 1, alignItems: 'flex-end'}}>
+            <Button
+              title="주소 확인"
+              onPress={handleButtonClick}
+              color="#B6BE6A" // 버튼 색상
+            />
+          </View>
         </View>
         <View>
-          <TextInput style={styles.Textinput} placeholder="강남대학교" />
+          <TextInput
+            style={styles.Textinput}
+            value={addressInput}
+            onChangeText={text => setAddressInput(text)}
+            placeholder="강남대학교"
+          />
         </View>
+        <View style={styles.View1}>
+          <Text style={{...styles.text, flex: 1}}>경도: {lat}</Text>
+          <Text style={{...styles.text, flex: 1}}>위도: {long}</Text>
+        </View>
+
         <View style={styles.View1}>
           <Ionicons name="time-outline" size={25} color={'black'} />
           <Text style={styles.text}>영업 시작 시간을 입력해 주세요</Text>
